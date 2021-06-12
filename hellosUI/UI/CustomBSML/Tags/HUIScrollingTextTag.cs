6b18f3f763674ab9e9e2bcc11c2d89dc22d20332
Chris <chrislee0419@gmail.com>
2021-06-12 22:43:54 +0100

    include ScrollingText component
    - kinda don't want ScrollingText to live in BSML anymore, since
      no one's going to use it, it's kinda inefficient, and if i ever
      need to update it, i don't have to go through BSML

---
+++ hellosUI/UI/CustomBSML/Tags/HUIScrollingTextTag.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using BeatSaberMarkupLanguage.Components;
+using BeatSaberMarkupLanguage.Tags;
+using HUI.UI.CustomBSML.Components;
+
+namespace HUI.UI.CustomBSML.Tags
+{
+    public class HUIScrollingTextTag : BSMLTag
+    {
+        public override string[] Aliases => new[] { "hui-scrolling-text" };
+
+        public override GameObject CreateObject(Transform parent)
+        {
+            GameObject gameObject = new GameObject("HUIScrollingText");
+            gameObject.transform.SetParent(parent, false);
+
+            HUIScrollingText scrollingText = gameObject.AddComponent<HUIScrollingText>();
+            scrollingText.rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+            scrollingText.rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
+            scrollingText.rectTransform.sizeDelta = new Vector2(90f, 6f);
+            scrollingText.textComponent.text = "Default Text";
+            scrollingText.textComponent.fontSize = 3f;
+            scrollingText.textComponent.color = Color.white;
+
+            LayoutElement layoutElement = gameObject.AddComponent<LayoutElement>();
+            layoutElement.preferredHeight = 6f;
+            layoutElement.preferredWidth = 90f;
+
+            ExternalComponents externalComponents = gameObject.AddComponent<ExternalComponents>();
+            externalComponents.components.Add(scrollingText.textComponent);
+
+            return gameObject;
+        }
+    }
+}
---
+++ hellosUI/UI/CustomBSML/TypeHandlers/HUIScrollingTextTypeHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using BeatSaberMarkupLanguage;
+using BeatSaberMarkupLanguage.TypeHandlers;
+using HUI.UI.CustomBSML.Components;
+
+namespace HUI.UI.CustomBSML.TypeHandlers
+{
+    [ComponentHandler(typeof(HUIScrollingText))]
+    public class HUIScrollingTextTypeHandler : TypeHandler<HUIScrollingText>
+    {
+        public override Dictionary<string, string[]> Props => new Dictionary<string, string[]>()
+        {
+            { "movementType", new[]{ "movement-type" } },
+            { "animationType", new[]{ "animation-type" } },
+            { "textWidthRatioThreshold", new[]{ "text-width-ratio-threshold" } },
+            { "pauseDuration", new[]{ "pause-duration" } },
+            { "scrollDuration", new[]{ "scroll-duration" } },
+            { "scrollSpeed", new[]{ "scroll-speed" } },
+            { "alwaysScroll", new[]{ "always-scroll" } },
+        };
+
+        public override Dictionary<string, Action<HUIScrollingText, string>> Setters => new Dictionary<string, Action<HUIScrollingText, string>>()
+        {
+            { "movementType", new Action<HUIScrollingText, string>((scrollingText, value) => scrollingText.movementType = (HUIScrollingText.ScrollMovementType)Enum.Parse(typeof(HUIScrollingText.ScrollMovementType), value)) },
+            { "animationType", new Action<HUIScrollingText, string>((scrollingText, value) => scrollingText.animationType = (HUIScrollingText.ScrollAnimationType)Enum.Parse(typeof(HUIScrollingText.ScrollAnimationType), value)) },
+            { "textWidthRatioThreshold", new Action<HUIScrollingText, string>((scrollingText, value) => scrollingText.textWidthRatioThreshold = Parse.Float(value)) },
+            { "pauseDuration", new Action<HUIScrollingText, string>((scrollingText, value) => scrollingText.pauseDuration = Parse.Float(value)) },
+            { "scrollDuration", new Action<HUIScrollingText, string>((scrollingText, value) => scrollingText.scrollDuration = Parse.Float(value)) },
+            { "scrollSpeed", new Action<HUIScrollingText, string>((scrollingText, value) => scrollingText.scrollSpeed = Parse.Float(value)) },
+            { "alwaysScroll", new Action<HUIScrollingText, string>((scrollingText, value) => scrollingText.alwaysScroll = Parse.Bool(value)) },
+        };
+    }
+}
