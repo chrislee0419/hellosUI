c0d84c5f815cb6b407af864ff3c371bfbfbd2ab2
Chris <chrislee0419@gmail.com>
2021-06-15 07:32:35 +0100

    version bump and manifest updates

--- SortModeExamplePlugin/Properties/AssemblyInfo.cs
+++ SortModeExamplePlugin/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SortModeExamplePlugin")]
-[assembly: AssemblyCopyright("Copyright © chrislee0419 2020")]
+[assembly: AssemblyCopyright("Copyright © chrislee0419 2020-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
